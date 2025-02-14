f4860385078a3a15f1ed38d6b3f489312c998ce4
eliia <eliia10@mail.ru>
2025-02-14 17:20:21 +0600

    fail

---
+++ py9.py
@@ -0,0 +1,12 @@
+import math
+import random
+a = int(input("Выстрел для каждого лучника: "))
+k = ['White', 'Green', 'Black']
+f = [(random.uniform(-10, 10), random.uniform(-10, 10) for i in range(), y for j(o))]
+
+r = {p: [math.sqrt(x**2 + y**2) for x ] for p in k}
+
+b = min(r.items(), key = lambda h: min(h[1]))
+l = min(r.items(), key = lambda h: sum(h[1]) / o )
+
+print("Точный выстрел у", {b[0]}, "с растоянием", {min(b[1])})
